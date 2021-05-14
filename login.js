b0307d50f38b2782bf839a286c02de28b2b3c677
Rohit Kodam <74586376+Rohitkk432@users.noreply.github.com>
2021-05-14 17:35:56 +0530

    added google signin and changed whole layout and color scheme

--- login.js
+++ login.js
@@ -1,1 +1,13 @@
-"use strict";
+let email;
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    email = profile.getEmail(); 
+    console.log(email);
+}
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log("User signed out.");
+    });
+}
