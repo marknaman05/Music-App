0d3e88bbc556c2f4af5b73dda16b54e7754aa5ad
sinhashlok <shlokjp@gmail.com>
2021-05-14 18:55:29 +0530

    changed nac bar + fixed nav for mobile view

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 "use strict";
+
+document.querySelector('.arrow-down').addEventListener('click', function () {
+    document.querySelector('.musicUi').classList.add('hidden');
+});
+
+
+document.querySelector('.hamburger').addEventListener('click', function () {
+    document.querySelector('.nav-tabs').classList.toggle('hidden1');
+});
